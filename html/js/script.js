bebe013b290f1e23d9f3fa2f373719af70fde3d7
Feiko Nanninga <admin@clonejo.de>
2016-02-06 18:43:32 +0100

    edit and delete announcements, escape username

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,62 @@
 "use strict";
-$(document).ready(function() {
 
 var api = "/api";
+
+var statuz = {
+    announcements: {}
+};
+statuz.announcement_edit = function(aid) {
+    var action = statuz.announcements[aid];
+    $("#announcement_modify_aid").val(aid);
+    $("#announcement_modify_from").val(moment.unix(action.from).format("L LT"));
+    $("#announcement_modify_to"  ).val(moment.unix(action.to  ).format("L LT"));
+    $("#announcement_modify_note").val(action.note);
+    $("#announcement_modify_box").slideDown();
+};
+
+statuz.announcement_stop = function(aid) {
+    var action = statuz.announcements[aid];
+    var user = $("#username").val();
+    if (user == "") {
+        user = "Hans Acker";
+    }
+    $.ajax(api + "/v0", {
+        data: JSON.stringify({
+            type: "announcement",
+            method: "mod",
+            user: user,
+            aid: aid,
+            from: action.from,
+            to: "now",
+            public: action.public
+        }),
+        method: "PUT",
+        success: function(res, status, jqxhr) {
+            statuz.update();
+        }
+    });
+}
+
+statuz.announcement_delete = function(aid) {
+    var user = $("#username").val();
+    if (user == "") {
+        user = "Hans Acker";
+    }
+    $.ajax(api + "/v0", {
+        data: JSON.stringify({
+            type: "announcement",
+            method: "del",
+            user: user,
+            aid: aid
+        }),
+        method: "PUT",
+        success: function(res, status, jqxhr) {
+            statuz.update();
+        }
+    });
+}
+
+$(document).ready(function() {
 
 // am/pm -> 24h and short fromNow
 moment.locale('en-24h', {
@@ -27,109 +82,128 @@ moment.locale('en-24h', {
     }
 });
 
-var update = function() {
-    console.log("updating");
-    $("#loading").css("visibility", "visible");
-    $("#status_box, #status_change_box, #announcement_box, #log_box").addClass("loading");
+statuz.update_status = function() {
+    $.getJSON(api + "/v0/status/current", function(o) {
+        var current_status = $("#current_status");
+        current_status.text(o.last.status);
+        if (o.last.status === "closed") {
+            current_status.removeClass();
+            current_status.addClass("closed");
+        } else if (o.last.status === "private") {
+            current_status.removeClass();
+            current_status.addClass("private");
+        } else if (o.last.status === "public") {
+            current_status.removeClass();
+            current_status.addClass("public");
+        }
+        $("#current_status_note").text(o.last.note);
+        if (o.last.note == "") {
+            $("#current_status_note_row").css("display", "none");
+        } else {
+            $("#current_status_note_row").css("display", "flex");
+        }
+        $("#status_change_note").val(o.last.note);
+        var changed = moment.unix(o.changed.time);
+        var updated = moment.unix(o.last.time)
+            $("#current_status_changed").text(changed.calendar() + " by " + o.changed.user + " (" + changed.fromNow(true) + ")");
+        $("#current_status_updated").text(updated.calendar() + " by " + o.last.user + " (" + updated.fromNow(true) + ")");
+        $("#status_box, #status_change_box").removeClass("loading");
+    });
+};
 
-    var requests = [
-        $.getJSON(api + "/v0/status/current", function(o) {
-            var current_status = $("#current_status");
-            current_status.text(o.last.status);
-            if (o.last.status === "closed") {
-                current_status.removeClass();
-                current_status.addClass("closed");
-            } else if (o.last.status === "private") {
-                current_status.removeClass();
-                current_status.addClass("private");
-            } else if (o.last.status === "public") {
-                current_status.removeClass();
-                current_status.addClass("public");
-            }
-            $("#current_status_note").text(o.last.note);
-            if (o.last.note == "") {
-                $("#current_status_note_row").css("display", "none");
-            } else {
-                $("#current_status_note_row").css("display", "flex");
-            }
-            $("#status_change_note").val(o.last.note);
-            var changed = moment.unix(o.changed.time);
-            var updated = moment.unix(o.last.time)
-                $("#current_status_changed").text(changed.calendar() + " by " + o.changed.user + " (" + changed.fromNow(true) + ")");
-            $("#current_status_updated").text(updated.calendar() + " by " + o.last.user + " (" + updated.fromNow(true) + ")");
-            $("#status_box, #status_change_box").removeClass("loading");
-        }),
-
-        $.getJSON(api + "/v0/status", function(o) {
-            var log_table = $("#log tbody");
-            log_table.empty();
-            $.each(o.actions, function(index, action) {
-                if (action.type == "status") {
-                    var row = '<tr>';
-                    row += "<td>" + action.id + "</td>";
-                    row += "<td>" + moment.unix(action.time).format("ddd, YYYY-MM-DD, LT") + "</td>";
-                    row += '<td><span class="status status_' + action.status + '">' + action.status + "</span></td>";
-                    row += "<td>by " + action.user + "</td>";
-                    row += '<td class="note">';
-                    if (action.note != "") {
-                        row += '“' + $("<div/>").text(action.note).html() + "”";
-                    }
-                    row += '</td>';
-                    row += "</tr>";
-                    log_table.prepend(row);
-                }
-            });
-            $("#log_box").removeClass("loading");
-        }),
-
-        $.getJSON(api + "/v0/announcement/current", function(o) {
-            var announcements_table = $("#announcements tbody");
-            announcements_table.empty();
-            $.each(o.actions, function(index, action) {
-                var calendarFormats = {
-                    sameElse: "ddd, YYYY-MM-DD, LT"
-                };
-                var from = moment.unix(action.from);
-                var to = moment.unix(action.to);
-                if (from.isAfter()) {
-                    var row = '<tr>';
-                } else {
-                    var row = '<tr class="begun">';
-                }
-                row += "<td>" + action.aid + "</td>";
-                row += '<td>';
-                if (!from.isAfter()) {
-                    row += '<i class="fa fa-play"></i> ';
-                }
-                row += from.calendar(null, calendarFormats) + "</td>";
-                row += "<td>" + to.calendar(null, calendarFormats) + "</td>";
-                row += "<td>by " + action.user + "</td>";
+statuz.update_log = function() {
+    $.getJSON(api + "/v0/status", function(o) {
+        var log_table = $("#log tbody");
+        log_table.empty();
+        $.each(o.actions, function(index, action) {
+            if (action.type == "status") {
+                var row = '<tr>';
+                row += "<td>" + action.id + "</td>";
+                row += "<td>" + moment.unix(action.time).format("ddd, YYYY-MM-DD, LT") + "</td>";
+                row += '<td><span class="status status_' + action.status + '">' + action.status + "</span></td>";
+                row += "<td>by " + $("<div />").text(action.user).html() + "</td>";
                 row += '<td class="note">';
                 if (action.note != "") {
                     row += '“' + $("<div/>").text(action.note).html() + "”";
                 }
                 row += '</td>';
-                if (action.public) {
-                    row += "<td>✔</td>";
-                }{
-                    row += "<td>&nbsp;</td>";
-                }
                 row += "</tr>";
-                announcements_table.append(row);
-            });
-            if (o.actions.length == 0) {
-                var row = '<tr><td colspan="6">no current or upcoming announcements</td></tr>';
-                announcements_table.append(row);
+                log_table.prepend(row);
             }
-            $("#announcement_box").removeClass("loading");
-        })
+        });
+        $("#log_box").removeClass("loading");
+    })
+};
+
+statuz.update_announcements = function() {
+    $.getJSON(api + "/v0/announcement/current", function(o) {
+        var announcements_table = $("#announcements tbody");
+        statuz.announcements = {};
+        announcements_table.empty();
+        $.each(o.actions, function(index, action) {
+            statuz.announcements[action.aid] = action;
+            var calendarFormats = {
+                sameElse: "ddd, YYYY-MM-DD, LT"
+            };
+            var from = moment.unix(action.from);
+            var to = moment.unix(action.to);
+            var begun = !from.isAfter();
+            if (begun) {
+                var row = '<tr class="begun">';
+            } else {
+                var row = '<tr>';
+            }
+            row += "<td>" + action.aid + "</td>";
+            row += '<td>';
+            if (begun) {
+                row += '<i class="fa fa-play"></i> ';
+            }
+            row += from.calendar(null, calendarFormats) + "</td>";
+            row += "<td>" + to.calendar(null, calendarFormats) + "</td>";
+            row += "<td>by " + $("<div />").text(action.user).html() + "</td>";
+            row += '<td class="note">';
+            if (action.note != "") {
+                row += '“' + $("<div/>").text(action.note).html() + "”";
+            }
+            row += '</td>';
+            if (action.public) {
+                row += "<td>✔</td>";
+            }{
+                row += "<td>&nbsp;</td>";
+            }
+            row += '<td class="actions">';
+            row += '<i class="fa fa-pencil" onclick="statuz.announcement_edit(' + action.aid + ')"></i> ';
+            if (begun) {
+                row += '<i class="fa fa-stop"  onclick="statuz.announcement_stop(' + action.aid + ')"></i>';
+            } else {
+                row += '<i class="fa fa-trash"  onclick="statuz.announcement_delete(' + action.aid + ')"></i>';
+            }
+            row += '</td>';
+            row += "</tr>";
+            announcements_table.append(row);
+        });
+        if (o.actions.length == 0) {
+            var row = '<tr><td colspan="6">no current or upcoming announcements</td></tr>';
+            announcements_table.append(row);
+        }
+        $("#announcement_box").removeClass("loading");
+    })
+};
+
+statuz.update = function() {
+    console.log("updating");
+    $("#loading").css("visibility", "visible");
+    $("#status_box, #status_change_box, #announcement_box, #log_box").addClass("loading");
+
+    var requests = [
+        statuz.update_status(),
+        statuz.update_log(),
+        statuz.update_announcements()
     ];
     $.when.apply($, requests).then(function() {
         $("#loading").css("visibility", "hidden");
     });
 };
-// debug:
-$("#header h1").click(function() { update() });
 
 var status_change = function(status) {
     $("#status_change_box").css("visibility", "hidden");
@@ -147,7 +221,7 @@ var status_change = function(status) {
         }),
         method: "PUT",
         success: function(res, status, jqxhr) {
-            update();
+            statuz.update();
             $("#status_change_box").css("visibility", "visible");
         }
     });
@@ -218,12 +292,42 @@ var announcement_add = function() {
         }),
         method: "PUT",
         success: function(res, status, jqxhr) {
-            update();
+            statuz.update();
             $("#announcement_add_box input[type=text]").val("");
             $("#announcement_add_box").css("visibility", "visible");
         }
     });
 };
+
+var announcement_modify = function() {
+    $("#announcement_modify_box").hide();
+    var user = $("#username").val();
+    if (user == "") {
+        user = "Hans Acker";
+    }
+    var note = $("#announcement_modify_note").val();
+    var time_range = parse_dates($("#announcement_modify_from").val(), $("#announcement_modify_to").val());
+    var aid = parseInt($("#announcement_modify_aid").val(), 10);
+    console.log("aid: " + aid);
+    $.ajax(api + "/v0", {
+        data: JSON.stringify({
+            type: "announcement",
+            method: "mod",
+            aid: aid,
+            user: user,
+            from: time_range[0],
+            to: time_range[1],
+            note: note
+        }),
+        method: "PUT",
+        success: function(res, status, jqxhr) {
+            statuz.update();
+        }
+    });
+};
+
+// debug:
+$("#header h1").click(function() { statuz.update() });
 
 $("#status_change_public" ).click(function(ev) { status_change("public" ); });
 $("#status_change_private").click(function(ev) { status_change("private"); });
@@ -234,6 +338,10 @@ $("#status_change_note_clear").click(function(ev) {
 });
 
 $("#announcement_add_submit").click(function(ev) { announcement_add() });
+$("#announcement_modify_submit").click(function(ev) { announcement_modify() });
+$("#announcement_modify_cancel").click(function(ev) {
+    $("#announcement_modify_box").slideUp();
+});
 
 var linkToLocalStorage = function(key, inputElem) {
     var inputElem = $(inputElem);
@@ -289,5 +397,5 @@ var toggleFullScreen = function() {
 }
 $("#fullscreen").click(toggleFullScreen);
 
-update();
+statuz.update();
 });
